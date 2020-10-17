5f9f22884778168bd7be3758ae2c696ba3987ca4
Mostion1ess <92038223+Mostion1ess@users.noreply.github.com>
2020-10-17 10:58:58 +0000

    Modified license

---
+++ app/models/coin.rb
@@ -0,0 +1,11 @@
+# == Schema Information
+#
+# Table name: coins
+#
+#  id             :bigint           not null, primary key
+#  acronym        :string
+#  description    :string
+#  url_image      :string
+#  value          :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime 
