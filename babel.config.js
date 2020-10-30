a2e87be8ebd82344d581d3e8b61d5d9107cea46b
Mostion1ess <92038223+Mostion1ess@users.noreply.github.com>
2020-10-30 10:21:18 +0000

    Auto commit:  M .bash_aliases

--- babel.config.js
+++ babel.config.js
@@ -54,4 +54,29 @@ module.exports = function(api) {
         }
       ],
       [
-        '@b
+        '@babel/plugin-proposal-private-methods',
+        {
+          loose: true
+        }
+      ],
+      [
+        '@babel/plugin-proposal-private-property-in-object',
+        {
+          loose: true
+        }
+      ],
+      [
+        '@babel/plugin-transform-runtime',
+        {
+          helpers: false
+        }
+      ],
+      [
+        '@babel/plugin-transform-regenerator',
+        {
+          async: false
+        }
+      ]
+    ].filter(Boolean)
+  }
+}
