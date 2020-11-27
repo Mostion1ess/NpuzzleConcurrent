130e82bfdc3acc185d69276ed649545df1a887f3
Mostion1ess <92038223+Mostion1ess@users.noreply.github.com>
2020-11-27 10:05:03 +0000

    ARM: 8426/1: dma-mapping: add missing range check in dma_mmap()

--- config/routes.rb
+++ config/routes.rb
@@ -1,1 +1,6 @@
-# == Rou
+# == Route Map
+#
+#                    Prefix Verb   URI Pattern                                                                              Controller#Action
+#                     coins GET    /coins(.:format)                                                                         coins#index
+#                           POST   /coins(.:format)                                                                         coins#create
+#                  new_coin GET    /coins/new(.:format)                                            
