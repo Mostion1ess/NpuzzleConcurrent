6a1c1975216313cd1442c854b53ca1f4f606921b
Mostion1ess <92038223+Mostion1ess@users.noreply.github.com>
2020-11-27 14:07:21 +0000

    Updated nav

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@
 #                    Prefix Verb   URI Pattern                                                                              Controller#Action
 #                     coins GET    /coins(.:format)                                                                         coins#index
 #                           POST   /coins(.:format)                                                                         coins#create
-#                  new_coin GET    /coins/new(.:format)                                            
+#                  new_coin GET    /coins/new(.:format)                                                                     coins#new
+#                 edit_coin GET    /coins/:id/edit(.:format)                                                                coins#edit
+#                      coin GET    /coins/:id(.:format)                                                                     coins#show
+#                           PATCH  /coins/:id(.:format)                                                                     coins#update
+#                           PUT 
