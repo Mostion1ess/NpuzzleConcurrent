28a0ce7e182cea52d2b972f0de8d8c1e739de83d
Mostion1ess <92038223+Mostion1ess@users.noreply.github.com>
2020-11-28 09:56:30 +0000

    py3 fixes

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,19 @@
 # rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
 #        rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
 # update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
-#      rails_direc
+#      rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
+
+Rails.application.routes.draw do
+  resources :mining_types
+  resources :coins do
+    member do
+      get 'mostrar'
+    end
+  end
+
+  get 'profile', to: 'mining_types#index'
+  root 'welcome#index'
+
+  # get 'mostrar' to: 'coins#mostrar'
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
