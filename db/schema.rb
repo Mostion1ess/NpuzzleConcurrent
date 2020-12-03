9b49659a3705804540f6bfc9339e434003b1fe62
Mostion1ess <92038223+Mostion1ess@users.noreply.github.com>
2020-12-03 12:30:23 +0000

    Fix build warnings

--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,24 @@
 ActiveRecord::Schema.define(version: 2022_01_19_134918) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extensio
+  enable_extension "plpgsql"
+
+  create_table "coins", force: :cascade do |t|
+    t.string "description"
+    t.string "acronym"
+    t.string "url_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "mining_type_id"
+    t.string "value"
+    t.index ["mining_type_id"], name: "index_coins_on_mining_type_id"
+  end
+
+  create_table "mining_types", force: :cascade do |t|
+    t.string "description"
+    t.string "acronym"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "coins",
