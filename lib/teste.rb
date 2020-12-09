7eeeab2bca36e4e4e92d48c8e2fc5d09900e12bf
Mostion1ess <92038223+Mostion1ess@users.noreply.github.com>
2020-12-09 06:21:36 +0000

    Automated continuous integration commit of minified JS/CSS

--- lib/teste.rb
+++ lib/teste.rb
@@ -11,4 +11,10 @@ headers = {
 
 headers: { 'authorization' => 'pk_26145608_LVDNV6LG8VP72C2N6WIQTZ1Y6HUXLUCV', 'content_type' => 'application/json'}
 
-# api = Api::B
+# api = Api::Base.new
+# @a = api.request(:get, "https://api.clickup.com/api/v2/list/25096231/task")
+
+# puts @a
+
+response = RestClient.get 'https://api.clickup.com/api/v2/task/294jdcc/', headers
+puts response
