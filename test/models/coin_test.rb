e0dda919417d45bb390b93c4203354feec657b2d
Mostion1ess <92038223+Mostion1ess@users.noreply.github.com>
2020-12-22 13:52:43 +0000

    whoops.

--- test/models/coin_test.rb
+++ test/models/coin_test.rb
@@ -2,4 +2,27 @@
 #
 # Table name: coins
 #
-#  id        
+#  id             :bigint           not null, primary key
+#  acronym        :string
+#  description    :string
+#  url_image      :string
+#  value          :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  mining_type_id :bigint
+#
+# Indexes
+#
+#  index_coins_on_mining_type_id  (mining_type_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (mining_type_id => mining_types.id)
+#
+require 'test_helper'
+
+class CoinTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
